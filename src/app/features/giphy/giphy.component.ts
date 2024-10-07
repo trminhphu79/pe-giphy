e3aa54fce16ab71b3648ea470014ee100a924883
Tran Minh Phu <trminhphu79@gmail.com>
2024-10-07 22:27:42 +0700

    Init source

---
+++ src/app/features/giphy/giphy.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-giphy',
+  standalone: true,
+  imports: [],
+  templateUrl: './giphy.component.html',
+  styleUrl: './giphy.component.scss'
+})
+export class GiphyComponent {
+
+}
