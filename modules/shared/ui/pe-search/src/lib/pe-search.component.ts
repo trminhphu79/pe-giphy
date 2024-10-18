e34bdb5ed65318f3fed13e306f0c6e032fbdcb01
Tran Minh Phu <trminhphu79@gmail.com>
2024-10-19 02:29:52 +0700

    Update some ui component

--- modules/shared/ui/pe-search/src/lib/pe-search.component.ts
+++ modules/shared/ui/pe-search/src/lib/pe-search.component.ts
@@ -1,21 +1,49 @@
-import { ChangeDetectionStrategy, Component, computed, ElementRef, input, Input, output, Output, signal, ViewChild } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { debounceTime, distinctUntilChanged, filter, fromEvent, interval, Subject, takeUntil, tap } from 'rxjs';
+import { AsyncPipe, NgForOf, NgIf, NgStyle } from '@angular/common';
+import { ChangeDetectorRef, QueryList, WritableSignal } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ElementRef, inject, input, output, signal, ViewChild, ViewChildren } from '@angular/core';
+import { debounceTime, delay, distinctUntilChanged, filter, fromEvent, interval, map, Observable, Subject, takeUntil, tap } from 'rxjs';
 import { TranslocoModule } from '@jsverse/transloco';
-import { getNextPlaceholderText, INTERVAL_UPDATE_PLACEHOLDER } from './pe-search.constant';
+import { getNextPlaceholderText, INTERVAL_UPDATE_PLACEHOLDER, MockItems } from './pe-search.constant';
 import { Channels, PeSearchData } from './pe-search.model';
 import { PeSearchType } from './pe-search.enum';
-import { NzPopoverModule } from 'ng-zorro-antd/popover';
-import { NzMentionModule } from 'ng-zorro-antd/mention';
+import { EMPTY_QUERY, TuiBooleanHandler } from '@taiga-ui/cdk';
+import {
+  TuiButton,
+  TuiDataList,
+  TuiDriver,
+  TuiDropdown,
+  tuiGetWordRange,
+  TuiIcon,
+  TuiInitialsPipe,
+  TuiOption,
+  TuiLoader
+} from '@taiga-ui/core';
+import { TuiAvatar, TuiChevron } from '@taiga-ui/kit';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TuiHighlight } from '@taiga-ui/kit';
+import { TuiTextareaModule } from '@taiga-ui/legacy';
 
 @Component({
   selector: 'pe-search',
   standalone: true,
   imports: [
-    CommonModule,
+    ReactiveFormsModule,
     TranslocoModule,
-    NzPopoverModule,
-    NzMentionModule
+    AsyncPipe,
+    FormsModule,
+    NgStyle,
+    NgForOf,
+    NgIf,
+    TuiAvatar,
+    TuiButton,
+    TuiChevron,
+    TuiDataList,
+    TuiDropdown,
+    TuiIcon,
+    TuiLoader,
+    TuiHighlight,
+    TuiTextareaModule,
+    TuiInitialsPipe
   ],
   templateUrl: './pe-search.component.html',
   styleUrl: './pe-search.component.scss',
@@ -27,25 +55,28 @@ export class PeSearchComponent {
   channels = input<Channels[]>();
   searchChanges = output<PeSearchData>();
 
-  showMention = signal(false);
   destroy$ = new Subject();
+
+  private cd: ChangeDetectorRef = inject(ChangeDetectorRef);
+
   @ViewChild('inputSearch', { static: true }) inputSearch!: ElementRef;
+  @ViewChildren(TuiOption, { read: ElementRef })
+  private readonly options: QueryList<ElementRef<HTMLElement>> = EMPTY_QUERY;
 
-  placeholderText = signal('SEARCH.PLACE_HOLDER.SEARCH_GIF')
 
-  inputValue?: string;
-  webFrameworks = [
-    { name: 'React', type: 'JavaScript' },
-    { name: 'Angular', type: 'JavaScript' },
-    { name: 'Laravel', type: 'PHP' },
-    { name: 'Flask', type: 'Python' },
-    { name: 'Django', type: 'Python' }
-  ];
+  protected readonly predicate: TuiBooleanHandler<Range> = (range) =>
+    tuiGetWordRange(range).toString().startsWith('@');
 
-  valueWith = (data: { name: string; type: string }): string => data.name;
 
-  onSelect(value: string): void {
-    console.log(value);
+  public readonly loading: WritableSignal<boolean> = signal(false);
+  public readonly dataSource: WritableSignal<Array<any>> = signal([])
+  public readonly dropdownOpen: WritableSignal<boolean> = signal(false);
+  public readonly searchControl: WritableSignal<FormControl> = signal(new FormControl(null));
+  public readonly placeholderText: WritableSignal<string> = signal('SEARCH.PLACE_HOLDER.SEARCH_GIF')
+
+
+  onSelect(value: any): void {
+    this.searchControl().setValue(value?.label)
   }
 
   ngOnInit() {
@@ -58,12 +89,36 @@ export class PeSearchComponent {
       )
       .subscribe()
 
+    this.searchControl().valueChanges
+      .pipe(
+        tap(() => this.loading.set(true)),
+        debounceTime(250),
+        distinctUntilChanged(),
+        map((v: string) => {
+          if (v[0] == '@') {
+            v.replace('@', '')
+          }
+
+          return v
+        }),
+        tap((value) => {
+          const filterValue = []
+          this.loading.set(filterValue.length == 0)
+        }),
+        delay(250),
+        tap((value) => {
+          const filterValue = MockItems.filter((item) => item.label.startsWith(value))
+          this.dataSource.set(filterValue)
+          this.loading.set(false)
+        })
+      )
+      .subscribe()
+
     fromEvent(this.inputSearch.nativeElement, 'keyup')
       .pipe(
         filter((event: any) => event.key == 'Enter'),
         distinctUntilChanged(),
         tap((event) => {
-          this.showMention.set(true)
           const keyword = event.target.value;
           const isSearchChannel = keyword?.[0] == '@';
           this.searchChanges.emit({
@@ -75,6 +130,14 @@ export class PeSearchComponent {
       .subscribe();
   }
 
+  protected onArrow(event: Event, which: 'first' | 'last'): void {
+    const item = this.options[which]
+    if (!item) {
+      return;
+    }
+    event.preventDefault();
+    item.nativeElement.focus();
+  }
 
   ngOnDestroy() {
     this.destroy$.next(null);
--- modules/shared/ui/pe-search/src/lib/pe-search.constant.ts
+++ modules/shared/ui/pe-search/src/lib/pe-search.constant.ts
@@ -3,3 +3,78 @@ export const INTERVAL_UPDATE_PLACEHOLDER = 3000;
 export const getNextPlaceholderText = (current: string): string => {
     return current == 'SEARCH.PLACE_HOLDER.SEARCH_GIF' ? 'SEARCH.PLACE_HOLDER.SEARCH_USERNAME' : 'SEARCH.PLACE_HOLDER.SEARCH_GIF'
 }
+
+export const MockItems = [
+    {
+        label: 'trminhphu79',
+        avatarUrl: 'https://media.licdn.com/dms/image/v2/D5603AQHm5c-_jdcBtg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1692433828078?e=1734566400&v=beta&t=zNDQU4t6JvE_pky0QDhaRIHOQXQi38cIPSsMY4fAV1c'
+    },
+    {
+        label: 'username33',
+        avatarUrl: 'https://media.licdn.com/dms/image/v2/D5603AQHm5c-_jdcBtg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1692433828078?e=1734566400&v=beta&t=zNDQU4t6JvE_pky0QDhaRIHOQXQi38cIPSsMY4fAV1c'
+    },
+    {
+        label: 'nguyenvanan',
+        avatarUrl: 'https://kenh14cdn.com/203336854389633024/2022/12/13/photo-2-16709308048291688699826.jpeg'
+    },
+    {
+        label: 'hochihtnanh',
+        avatarUrl: 'https://media.licdn.com/dms/image/v2/D5603AQHm5c-_jdcBtg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1692433828078?e=1734566400&v=beta&t=zNDQU4t6JvE_pky0QDhaRIHOQXQi38cIPSsMY4fAV1c'
+    },
+    {
+        label: 'chihthanh2',
+        avatarUrl: 'https://t3.ftcdn.net/jpg/05/76/94/70/360_F_576947051_DFT5rJEsF8yturr1DOlB3rxhtxswGSmP.jpg'
+    },
+    {
+        label: 'chihthanh3',
+        avatarUrl: 'https://t4.ftcdn.net/jpg/05/66/63/19/360_F_566631993_wTzG2ryyNj6gbvdwfYEVxQNCDcwz0ba7.jpg'
+    },
+    {
+        label: 'chihthanh4',
+        avatarUrl: 'https://media.licdn.com/dms/image/v2/D5603AQHm5c-_jdcBtg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1692433828078?e=1734566400&v=beta&t=zNDQU4t6JvE_pky0QDhaRIHOQXQi38cIPSsMY4fAV1c'
+    },
+    {
+        label: 'minhphu',
+        avatarUrl: 'https://t3.ftcdn.net/jpg/05/76/94/70/360_F_576947051_DFT5rJEsF8yturr1DOlB3rxhtxswGSmP.jpg'
+    },
+    {
+        label: 'vantroi',
+        avatarUrl: 'https://media.licdn.com/dms/image/v2/D5603AQHm5c-_jdcBtg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1692433828078?e=1734566400&v=beta&t=zNDQU4t6JvE_pky0QDhaRIHOQXQi38cIPSsMY4fAV1c'
+    },
+    {
+        label: 'trminhphu79',
+        avatarUrl: 'https://media.licdn.com/dms/image/v2/D5603AQHm5c-_jdcBtg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1692433828078?e=1734566400&v=beta&t=zNDQU4t6JvE_pky0QDhaRIHOQXQi38cIPSsMY4fAV1c'
+    },
+    {
+        label: 'username33',
+        avatarUrl: 'https://media.licdn.com/dms/image/v2/D5603AQHm5c-_jdcBtg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1692433828078?e=1734566400&v=beta&t=zNDQU4t6JvE_pky0QDhaRIHOQXQi38cIPSsMY4fAV1c'
+    },
+    {
+        label: 'nguyenvanan',
+        avatarUrl: 'https://kenh14cdn.com/203336854389633024/2022/12/13/photo-2-16709308048291688699826.jpeg'
+    },
+    {
+        label: 'hochihtnanh',
+        avatarUrl: 'https://media.licdn.com/dms/image/v2/D5603AQHm5c-_jdcBtg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1692433828078?e=1734566400&v=beta&t=zNDQU4t6JvE_pky0QDhaRIHOQXQi38cIPSsMY4fAV1c'
+    },
+    {
+        label: 'chihthanh2',
+        avatarUrl: 'https://t3.ftcdn.net/jpg/05/76/94/70/360_F_576947051_DFT5rJEsF8yturr1DOlB3rxhtxswGSmP.jpg'
+    },
+    {
+        label: 'chihthanh3',
+        avatarUrl: 'https://t4.ftcdn.net/jpg/05/66/63/19/360_F_566631993_wTzG2ryyNj6gbvdwfYEVxQNCDcwz0ba7.jpg'
+    },
+    {
+        label: 'chihthanh4',
+        avatarUrl: 'https://media.licdn.com/dms/image/v2/D5603AQHm5c-_jdcBtg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1692433828078?e=1734566400&v=beta&t=zNDQU4t6JvE_pky0QDhaRIHOQXQi38cIPSsMY4fAV1c'
+    },
+    {
+        label: 'minhphu',
+        avatarUrl: 'https://t3.ftcdn.net/jpg/05/76/94/70/360_F_576947051_DFT5rJEsF8yturr1DOlB3rxhtxswGSmP.jpg'
+    },
+    {
+        label: 'vantroi',
+        avatarUrl: 'https://media.licdn.com/dms/image/v2/D5603AQHm5c-_jdcBtg/profile-displayphoto-shrink_800_800/profile-displayphoto-shrink_800_800/0/1692433828078?e=1734566400&v=beta&t=zNDQU4t6JvE_pky0QDhaRIHOQXQi38cIPSsMY4fAV1c'
+    },
+];
