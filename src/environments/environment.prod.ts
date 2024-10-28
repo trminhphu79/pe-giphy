7ebdaa7effaca2f670a8acfdcaa8d78f722070de
Tran Minh Phu <trminhphu79@gmail.com>
2024-10-28 20:21:48 +0700

    Enhance searching and my gifs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { AppConfigState } from "@pe-giphy/app-config";
 export const environment: AppConfigState = {
     apiUrl: 'https://api.giphy.com/',
     uploadUrl: 'https://upload.giphy.com/',
-    apiKey: 'ye0xOgys9joX91Eat4LWtu9GmMugq629',
+    apiKey: 'oayBj2OyX9gmC2a2nMMruIsLiGEXgqHH',
     apiVersion: 'v1',
     version: '1.0.0'
 }
